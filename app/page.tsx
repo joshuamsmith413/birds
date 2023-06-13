8b7f38eed0e7cfeeb9119269379c606c0e02a62d
josh smith <joshsmith@joshs-MacBook-Pro.local>
2023-06-13 15:22:31 -0400

    first commit, view common birds in one area

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react';
+import styles from './page.module.css';
+
+type TBirdData = {
+  speciesCode: string;
+  comName: string;
+  sciName: string;
+  locId: string;
+  locName: string;
+  obsDt: string;
+  howMany: number;
+  lat: number;
+  lng: number;
+  obsValid: boolean;
+  obsReviewed: boolean;
+  locationPrivate: boolean;
+  subId: string;
+  subnational2Code: string;
+  subnational2Name: string;
+  subnational1Code: string;
+  subnational1Name: string;
+  countryCode: string;
+  countryName: string;
+  userDisplayName: string,
+  obsId: string,
+  checklistId: string,
+  presenceNoted: boolean,
+  hasComments: boolean,
+  lastName: string,
+  firstName: string,
+  hasRichMedia: boolean
+};
+
+const HTTPS_HEADERS = {
+  'X-eBirdApiToken': `${process.env.BIRD_KEY}`,
+};
+
+const BASE_URL = 'https://api.ebird.org/v2';
+const HEADERS = new Headers(HTTPS_HEADERS);
+
+async function getSpeciesByRegion(regionCode = 'US-NY-053') {
+  const res = await fetch(`${BASE_URL}/product/spplist/${regionCode}`, {
+    method: 'GET',
+    headers: HEADERS,
+    redirect: 'follow'
+  })
+  return res.json();
+}
+
+async function getNotableSitingByRegion(regionCode = 'US-NY-053') {
+  const res = await fetch(`${BASE_URL}/data/obs/${regionCode}/recent/notable?detail=full`, {
+    method: 'GET',
+    headers: HEADERS,
+    redirect: 'follow'
+  });
+  return res.json();
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const fetchSitingsData = async () => {
+    const result: TBirdData[] = await getNotableSitingByRegion();
+    // console.log('result', result);
+    return result;
+  };
+
+  const fetchSpeciesByRegion = async () => {
+    const result = await getSpeciesByRegion();
+    console.log('result',result)
+    return result
+  }
+
+  const showSpecies = async () => {
+    const birdData: TBirdData[] = await fetchSitingsData();
+    return birdData.map((data, index) => (
+        <div className={styles.card} key={index}>
+          <h4>{data.comName}</h4>
+          <p className={styles.description}>{data.subnational2Name}, {data.subnational1Name}</p>
         </div>
+      ));
+    }
+
+  return (
+    <main className={`${styles.grid} ${styles.main}`}>
+      <div>
+        <h3>Recent Notable Sitings</h3>
+        {showSpecies()}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <h3>Species List For a Region</h3>
+        {fetchSpeciesByRegion()}
       </div>
     </main>
-  )
+  );
 }
